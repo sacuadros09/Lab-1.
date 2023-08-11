3e4e2b2db1cd323eb6c0e29a7d79ee1ed11b040a
sacuadros09 <sacuma09@gmail.com>
2023-08-11 17:19:15 -0500

    Lab 1 terminado

---
+++ index.js
@@ -0,0 +1,5 @@
+const reverse = require('reverse-geocode')
+console.log(reverse.lookup(37.8072792, -122.4780652, 'us'))
+
+var random_name = require('node-random-name');
+console.log(random_name());
